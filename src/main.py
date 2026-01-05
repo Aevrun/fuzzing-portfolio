757117e793a915b05cfc02e81715ac1b61446b2c
Aevrun <aevrun@gmail.com>
2026-01-05 15:59:43 +0100

    ignore for now

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    print(f"Python version: {sys.version}")
+    print(f"Operating_System: {os.name}")
+    print("Targeting 2026: Abhijeet is ready")
